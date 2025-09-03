7611f1e3d8e2f6ad3e4433db8135ff689ec8f839
PETER MUSSA <nassoropeter97@gmail.com>
2025-09-03 21:17:07 +0300

    add changes

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,7 @@
-const UserProfile = (props) => {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
-    </div>
-  );
-};
+import UserInfo from './UserInfo';
 
-export default UserProfile;
+function ProfilePage() {
+  return <UserInfo />;
+}
+
+export default ProfilePage;
