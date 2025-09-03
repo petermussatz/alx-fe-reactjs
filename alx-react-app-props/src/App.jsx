2c5410ab4bed007b81cb6c86e212e23b1662d596
PETER MUSSA <nassoropeter97@gmail.com>
2025-09-03 21:43:18 +0300

    add changes

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,9 +1,16 @@
 import ProfilePage from './ProfilePage';
+import UserContext from './UserContext'; // import the context
 
 function App() {
+  //  this is your user data
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return <ProfilePage userData={userData} />;
+  return (
+    //  wrap your app with the Provider and pass value
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
 
 export default App;
