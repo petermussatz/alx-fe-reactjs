330b41f728c52743afd6322b6b10dc246a708537
PETER MUSSA <nassoropeter97@gmail.com>
2025-09-03 21:06:50 +0300

    add changes

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,43 @@
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+    </>
+  );
+}
+
+export default App;
+
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+  return (
+    <>
+      <WelcomeMessage />
+    </>
+  );
+}
+
+export default App;
+
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
